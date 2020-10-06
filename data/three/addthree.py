0cd22f734fc8ef586895a4073c658329254ad9b3
Chen <chen@localhost.localdomain>
2020-10-06 18:40:33 +0800

    18点40分

--- data/three/addthree.py
+++ data/three/addthree.py
@@ -1,11 +1,11 @@
 f = open('three.nt','w',encoding='utf-8')
 
-for i in range(1,1001):
+for i in range(1,5001):
    f.write('<v'+str(i)+'>   <a>   <v>.\n')
 
-for i in range(1001,2001):
+for i in range(5001,10001):
    f.write('<v>   <a>   <v'+str(i)+'>.\n')
 
-f.write('<v2000>   <a>   <v\'>.')
+f.write('<v10000>   <a>   <v\'>.')
 
 f.close()
