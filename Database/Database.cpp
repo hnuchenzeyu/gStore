18580f3cec4d06f418a97261d1aa999cb5a6e4f8
czy <553101282@qq.com>
2020-10-06 18:43:28 +0800

    18点43分

--- Database/Database.cpp
+++ Database/Database.cpp
@@ -1622,7 +1622,7 @@ Database::query(const string _query, ResultSet& _result_set, FILE* _fp, bool upd
         ans_num = min(ans_num, (long long)_result_set.output_limit);
       cout << "There has answer: " << ans_num << endl;
       cout << "final result is : " << endl;
-      _result_set.output(_fp);
+      // _result_set.output(_fp); // 暂时不显示结果
       fprintf(_fp, "\n");
       fflush(_fp); //to empty the output buffer in C (fflush(stdin) not work in GCC)
     }
