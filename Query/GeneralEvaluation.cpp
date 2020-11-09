793159784caa20ecb13edfa3472752bb39083913
Ubuntu_in_lab <553101282@qq.com>
2020-11-09 01:14:44 -0800

    remove previous method of jumping like join based kvstore.

--- Query/GeneralEvaluation.cpp
+++ Query/GeneralEvaluation.cpp
@@ -128,21 +128,8 @@ bool GeneralEvaluation::doQuery()
     this->rewriting_evaluation_stack.push_back(EvaluationStackStruct());
     this->rewriting_evaluation_stack.back().group_pattern = this->query_tree.getGroupPattern();
     this->rewriting_evaluation_stack.back().result = NULL;
-
-// =====================Jumping like join==================================================
-    cout<< "this->kvstore: " << this->kvstore <<endl;
-    JumpingLikeJoin* jumpingLikeJoin = new JumpingLikeJoin(this->kvstore);
-    cout << jumpingLikeJoin->getPreID("<http://dbpedia.org/ontology/child>") <<endl;
-    long t1 = Util::get_cur_time();
-    jumpingLikeJoin->initSubObjListMap(jumpingLikeJoin->getPreID("<http://dbpedia.org/ontology/child>"));
-    long t2 = Util::get_cur_time();
-    cout<< "czy: after init map, used "<< t2 -t1 << "ms" <<endl;
-    this->temp_result = jumpingLikeJoin->intersect();
-    long t3 = Util::get_cur_time();
-    cout<< "czy: after intersect, used "<< t3 -t2 << "ms" <<endl;
-// =====================Jumping like join==================================================
       
-    // this->temp_result = this->rewritingBasedQueryEvaluation(0);
+    this->temp_result = this->rewritingBasedQueryEvaluation(0);
 
   } else {
     printf("=====================\n");
